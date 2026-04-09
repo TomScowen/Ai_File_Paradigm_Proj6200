266507b67ece689f22b39fa7bb7bd1ed61311822
TomScowen <tomscowen@gmail.com>
2026-04-09 14:56:12 +0100

    Adding the app py, to develope first iteration of UI

---
+++ core/fileParadigmApp.py
@@ -0,0 +1,17 @@
+#  ___________________________________________________________________
+# |=-=-=-=-=-=-=-=-=-=-=-=-=-| PHASE  FOUR |-=-=-=-=-=-=-=-=-=-=-=-=-=|
+# | FILE PARADIGM APP > This section is dedicated to the Streamlit UI |
+# | for the application. Where the UI will allow for file uploading,  |
+# | passed through the other phases and outputting a newly generated. |
+# | file made system.                                                 |
+# |                                                                   |
+# |                                                                   |
+# |           The Core UI and Direction of the Application.           |
+#  \_________________________________________________________________/
+
+
+# IMPORT LIST ----> 
+import streamlit # for building the user interface
+import sys # pythons built in sys library (needs to be modified python path for core folder)
+import os # pythons built-in operating system (needs to build file path for core folder)
+
